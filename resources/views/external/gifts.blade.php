601f71680d917ac897e1c9b1b7c2934e138aa615
wingerlion <a20101616@pucp.pe>
2015-09-23 15:46:02 -0500

    canjeo de juguetes interno y externo

--- resources/views/external/gifts.blade.php
+++ resources/views/external/gifts.blade.php
@@ -1,6 +1,8 @@
 @extends('layoutExternal')
 
 @section('style')
+	{!!Html::style('css/skeletoGift.css')!!}
+	{!!Html::style('css/estiloGift.css')!!}
 	
 @stop
 
@@ -9,7 +11,76 @@
 @stop
 
 @section('content')
-	regalos
+		<header>
+					<h1>Canjea tu Premio </h1>
+
+					<p><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur  0.1sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</header>
+				<div class="row">
+					<div class="4u">
+						<section style="text-align:center;">
+							 <font size="6">250 Puntos</font><a href="#" class="image full"><img src="images/pics01.jpg" alt="" />Death Prophet </a> 
+
+						</section>
+					</div>
+					<div class="4u">
+						<section style="text-align:center;">
+							 <font size="6">950 Puntos</font><a href="#" class="image full"><img src="images/img3315jpg-67c048_765w.jpg" alt="" />Blood Seeker</a>
+
+						</section>
+					</div>
+					<div class="4u">
+						<section style="text-align:center;">
+							 <font size="6">330 Puntos</font><a href="#" class="image full"><img src="images/fb195e148b402e8594df0e080050b3c10da0ea47a87e09ea13bd6de921b66945_large.jpg" alt="" />Bounty Hunter</a>
+
+						</section>
+					</div>
+				</div>
+				<div class="row">
+					<div class="4u">
+						<section style="text-align:center;">
+							 <font size="6">1000 Puntos</font><a href="#" class="image full"><img src="images/void.png" alt="" />Void</a>
+
+						</section>
+					</div>
+					<div class="4u">
+						<section style="text-align:center;">
+							 <font size="6">150 Puntos</font><a href="#" class="image full"><img src="images/pudge.png" alt="" />Rilay</a>
+
+						</section>
+					</div>
+					<div class="4u">
+						<section style="text-align:center;">
+						 <font size="6">780 Puntos</font>	<a href="#" class="image full"><img src="images/rilay.png" alt="" />Pudge</a>
+
+						</section>
+					</div>
+				</div>
+				<div class="row">
+					<div class="4u">
+						<section style="text-align:center;">
+						 <font size="6">150 Puntos</font>	<a href="#" class="image full"><img src="images/tiny.png" alt="" />Tiny</a>
+
+						</section>
+					</div>
+					<div class="4u">
+						<section style="text-align:center;">
+						 <font size="6">190 Puntos</font>	<a href="#" class="image full"><img src="images/venge.png" alt="" />Vengeful</a>
+
+						</section>
+					</div>
+					<div class="4u">
+						<section style="text-align:center;">
+						 <font size="6">210 Puntos</font>	<a href="#" class="image full"><img src="images/slark.png" alt="" />Slark</a>
+
+						</section>
+					</div>
+				</div>
 @stop
 
 @section('javascript')
