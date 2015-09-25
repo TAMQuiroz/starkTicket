a426130fc3245747b8eba317fd0c39e29a76bc40
Juan E <hm.erajuan@gmail.com>
2015-09-25 12:47:54 -0500

    He agregado formulario de agregar devolucion

--- resources/views/internal/admin/newModule.blade.php
+++ resources/views/internal/admin/newModule.blade.php
@@ -9,59 +9,58 @@
 @stop
 
 @section('content')
-        <!-- Contenido-->
-        <div class="row">
-          <div class="col-sm-8">
-            <form class="form-horizontal" method="post">
-              <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="inputEmail3" placeholder="">
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">Direción</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="inputEmail3" placeholder="">
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">Departamento</label>
-                <div class="col-sm-10">
-                  <select class="form-control">
-                    <option>Departamento 1</option>
-                    <option>Departamento 2</option>
-                    <option>Departamento 3</option>
-                    <option>Departamento 4</option>
-                  </select>
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">Provincia</label>
-                <div class="col-sm-10">
-                  <select class="form-control">
-                    <option>Provincia 1</option>
-                    <option>Provincia 2</option>
-                    <option>Provincia 3</option>
-                    <option>Provincia 4</option>
-                  </select>
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">Imagen</label>
-                <div class="col-sm-10">
-                  <input type="file" class="form-control" id="inputEmail3" placeholder="">
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                  <a class="btn btn-primary"  data-toggle="modal" data-target="#agregado">Guardar</a>
-                  <button type="reset" class="btn btn-danger">Cancelar</button>
-                </div>
-              </div>
-            </form>
-          </div>
+<div class="row">
+  <div class="col-sm-8">
+    <form class="form-horizontal" method="post">
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="inputEmail3" placeholder="">
         </div>
+      </div>
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Direción</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="inputEmail3" placeholder="">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Departamento</label>
+        <div class="col-sm-10">
+          <select class="form-control">
+            <option>Departamento 1</option>
+            <option>Departamento 2</option>
+            <option>Departamento 3</option>
+            <option>Departamento 4</option>
+          </select>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Provincia</label>
+        <div class="col-sm-10">
+          <select class="form-control">
+            <option>Provincia 1</option>
+            <option>Provincia 2</option>
+            <option>Provincia 3</option>
+            <option>Provincia 4</option>
+          </select>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Imagen</label>
+        <div class="col-sm-10">
+          <input type="file" class="form-control" id="inputEmail3" placeholder="">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <a class="btn btn-primary"  data-toggle="modal" data-target="#agregado">Guardar</a>
+          <button type="reset" class="btn btn-danger">Cancelar</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
 @stop
 
 @section('javascript')
--- resources/views/internal/admin/newTicketReturn.blade.php
+++ resources/views/internal/admin/newTicketReturn.blade.php
@@ -9,7 +9,30 @@
 @stop
 
 @section('content')
-
+<div class="row">
+  <div class="col-sm-8">
+    <form class="form-horizontal" method="post">
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Cliente</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="inputEmail3" placeholder="">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Ticket</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="inputEmail3" placeholder="">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <a class="btn btn-primary"  data-toggle="modal" data-target="#agregado">Guardar</a>
+          <button type="reset" class="btn btn-danger">Cancelar</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
 @stop
 
 @section('javascript')
