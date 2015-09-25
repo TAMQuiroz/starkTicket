e47ae1c9264c352acaa30da62699b3a1652b4c2d
Andrea <axbravo@pucp.pe>
2015-09-25 18:19:03 -0500

    reporte de asignaciones terminado

--- resources/views/internal/admin/reports/assignment.blade.php
+++ resources/views/internal/admin/reports/assignment.blade.php
@@ -11,14 +11,24 @@
 @section('content')
 <div class="row">
     <div class="col-sm-3">
-        <label>Seleccione al vendedor</label>
+        <label>Seleccione el punto de venta</label>
         <select class="form-control">
-          <option value="volvo">Todos los vendedores</option>
-          <option value="saab">Juan Perez</option>
-          <option value="opel">Ana García</option>
-          <option value="audi">Miguel Guanira</option>
+          <option value="volvo">Todos los puntos de venta</option>
+          <option value="saab">La Molina</option>
+          <option value="opel">San Borja</option>
+          <option value="audi">La Perla</option>
         </select>
+        
     </div>
+     <div class="col-sm-3">
+    <label>Seleccione el tipo de empleado</label>
+        <select class="form-control">
+          <option value="volvo">Todos los empleados</option>
+          <option value="saab">Vendedores</option>
+          <option value="opel">Promotores de Venta</option>
+          <option value="audi">Administradores</option>
+        </select>
+         </div>
     <div class="col-sm-2">
         <label>Desde</label>
         <input type="date" class="form-control">
@@ -26,14 +36,6 @@
     <div class="col-sm-2">
         <label>Hasta</label>
         <input type="date" class="form-control">
-    </div>
-    <div class="col-sm-2">
-        <label>Tipo</label>
-        <select class="form-control">
-          <option value="opel">Tabla</option>
-          <option value="volvo">Excel</option>
-          <option value="saab">PDF</option>
-        </select>
     </div>
     <div class="col-sm-3">
         <br>
@@ -46,35 +48,21 @@
 <div id="demo2" class="collapse">
     <table class="table table-bordered table-striped">
         <tr>
-            <th>Apellidos y Nombres</th>
-            <th>DNI</th>
-            <th>Telefonos(s)</th>
-            <th>Sexo</th>
+            <th>Punto de Venta</th>
+            <th>Nombres y Apellidos</th>
+            <th>Tipo de Empleado</th>
         </tr>
         <tr>
-            <td>Contenido 1</td>
-            <td>Contenido 2</td>
-            <td>Contenido 3</td>
-            <td>Contenido 4</td>
+            <td>La Molina</td>
+            <td>Pepito Marquez</td>
+            <td>Vendedor</td>
         </tr>
         <tr>
-            <td>Contenido 1</td>
-            <td>Contenido 2</td>
-            <td>Contenido 3</td>
-            <td>Contenido 4</td>
+            <td>San Borja</td>
+            <td>Wendy Perez</td>
+            <td>Administrador</td>
         </tr>
-        <tr>
-            <td>Contenido 1</td>
-            <td>Contenido 2</td>
-            <td>Contenido 3</td>
-            <td>Contenido 4</td>
-        </tr>
-        <tr>
-            <td>Contenido 1</td>
-            <td>Contenido 2</td>
-            <td>Contenido 3</td>
-            <td>Contenido 4</td>
-        </tr>
+
     </table>
 </div>
 
