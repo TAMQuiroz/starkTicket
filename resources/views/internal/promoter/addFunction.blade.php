3cf99692956f973ce871dcbb4aea514c3cdc928e
valeria <ug1123@hotmail.com>
2015-10-02 16:49:30 -0500

    crear evento-promotor

--- resources/views/internal/promoter/addFunction.blade.php
+++ resources/views/internal/promoter/addFunction.blade.php
@@ -12,8 +12,132 @@
 
 
         <div class="row">
-          <div class="col-sm-8">
+          <div class="col-sm-8"> 
             <form class="form-horizontal" method="post">
+
+                <legend>Agregar precios:</legend>
+
+                <div class="row">
+                    
+                    <div class="col-sm-3">
+                        <label>Desde</label>
+                        <input type="date" class="form-control">
+                    </div>
+                    <div class="col-sm-3">
+                        <label>Hasta</label>
+                        <input type="date" class="form-control">
+                    </div>
+                    <div class="col-sm-3">
+                        <label>Zona</label>
+                        <select class="form-control">
+                          <option value="saab">Vip</option>
+                          <option value="opel">Platea</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-2">
+                        <label>Cliente</label>
+                        <select class="form-control">
+                          <option value="opel">Nino</option>
+                          <option value="volvo">Adulto</option>
+                          <option value="saab">Adulto mayor</option>
+                        </select>
+                    </div>         
+                        
+                    <!--
+                    <div class="col-sm-3">
+                        <br>
+                        <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo2">Mostrar Reporte</button>
+                    </div>
+                    -->
+                </div>
+                <br>
+                <div class="row">
+                    <label for="inputEmail3" class="col-sm-1 control-label">Precio</label>
+                    <div class="col-sm-3">
+                      <input type="text" class="form-control" id="inputEmail3" placeholder="">
+                    </div>
+                </div>
+                <br>
+
+                <div class="form-group">
+                    <div class="col-sm-offset-10 col-sm-10">
+                        <a class="btn btn-info" onclick="addZone()">Agregar</a>
+                        <!-- 
+                        <button  type="reset" class="btn btn-info">Cancelar</button>
+                        -->
+                    </div>
+                </div>
+
+                <script>
+
+                    function addClient(){
+
+                        var cl = document.getElementById('input-client').value;
+
+                        var tableRef = document.getElementById('table-client').getElementsByTagName('tbody')[0];
+
+                        // Insert a row in the table at the last row
+                        var newRow   = tableRef.insertRow(tableRef.rows.length);
+
+                        // Insert a cell in the row at index 0
+                        var newCell  = newRow.insertCell(0);
+                        var newCell2 = newRow.insertCell(1);
+                        var newCell3 = newRow.insertCell(2);
+
+                        // Append values to cells
+                        var newText  = document.createTextNode(cl);
+                        
+                        // buttons
+                        var newEdit = document.createElement('button');
+                        var newDelete = document.createElement('button');
+                        newEdit.className = "btn"; 
+                        newEdit.className += " btn-info glyphicon glyphicon-pencil"; 
+                        newDelete.className = "btn";
+                        newDelete.className += " btn-info glyphicon glyphicon-remove";
+
+                        newCell.appendChild(newText);                        
+                        newCell2.appendChild(newEdit);
+                        newCell3.appendChild(newDelete);
+                
+                    }
+
+                </script>
+
+                <table id="table-client" class="table table-bordered table-striped ">
+                    <tr>
+                        <th>Zona</th>
+                        <th>Tipo de cliente</th>
+                        <th>Precio</th>
+                        <th>Editar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                    <tr>
+                        <td>Vip</td>
+                        <td>Adulto</td>   
+                        <td>350</td>               
+                        <td>
+                            <a class="btn btn-info"  title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
+                        </td>   
+                        <td>
+                             <a class="btn btn-info"   title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
+                         </td>
+                    </tr>
+                    <tr>
+                        <td>Platea</td>                        
+                        <td>Nino</td>
+                        <td>90</td>
+                        <td>
+                            <a class="btn btn-info" title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
+                        </td>   
+                        <td>
+                             <a class="btn btn-info"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
+                         </td>
+                    </tr>            
+
+                </table>
+
+
+                <!--
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Fecha inicio</label>
                     <div class="col-sm-10">
@@ -50,109 +174,25 @@
                         </select>
                     </div>            
                 </div>  
+                -->
+
+
+                <!--
                 <legend>Agregar zona:</legend>
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="">
+                        <input type="text" class="form-control" id="input-zone" placeholder="">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Capacidad</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="">
+                        <input type="text" class="form-control" id="input-capacity" placeholder="">
                     </div>
                 </div>  
-
-                <table class="table table-bordered table-striped ">
-                    <tr>
-                        <th></th>
-                        <th>Precio al 05/10/2015</th>
-                        <th>Precio al 10/10/2015</th>
-                        <th>Precio estándar</th>
-                        <th>Agregar</th>
-                        <th>Eliminar</th>
-                    </tr>
-                    <tr>
-                        <th>Adulto</th>
-                        <td>150.0</td>
-                        <td>250.0</td>
-                        <td>350.0</td>
-                        <td>
-                            <a class="btn btn-info" title="Agregar"><i class="glyphicon glyphicon-plus"></i></a>
-                        </td>
-                        <td>
-                            <a class="btn btn-info"  title="Eliminar"><i class="glyphicon glyphicon-remove"></i></a>
-                        </td>                         
-                    </tr>
-                    <tr>
-                        <th>Niños</th>
-                        <td>150.0</td>
-                        <td>250.0</td>
-                        <td>300.0</td>
-                        <td>
-                            <a class="btn btn-info" title="Agregar"><i class="glyphicon glyphicon-plus"></i></a>
-                        </td>
-                        <td>
-                            <a class="btn btn-info"  title="Eliminar"><i class="glyphicon glyphicon-remove"></i></a>
-                        </td>                         
-                    </tr>       
-                    <tr>
-                        <th>Adulto mayor</th>
-                        <td>150.0</td>
-                        <td>250.0</td>
-                        <td>310.0</td>
-                        <td>
-                            <a class="btn btn-info" title="Agregar"><i class="glyphicon glyphicon-plus"></i></a>
-                        </td>
-                        <td>
-                            <a class="btn btn-info"  title="Eliminar"><i class="glyphicon glyphicon-remove"></i></a>
-                        </td>                         
-                    </tr>                                    
-
-                </table>    
-
-                <div class="form-group">
-                    <div class="col-sm-offset-8 col-sm-10">
-                        <a class="btn btn-info" >Agregar zona</a>
-                        <button  type="reset" class="btn btn-info">Cancelar</button>
-                    </div>
-                </div>                                
-
-                <table class="table table-bordered table-striped ">
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Capacidad</th>
-                        <th>Precio estándar</th>
-                        <th>Editar</th>
-                        <th>Eliminar</th>
-                    </tr>
-                    <tr>
-                        <td>Zona vip</td>
-                        <td>200</td>
-                        <td>350.0</td>
-                        <td>
-                            <a class="btn btn-info"  title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
-                        </td>   
-                        <td>
-                             <a class="btn btn-info"   title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
-                         </td>
-                    </tr>
-                    <tr>
-                        <td>Zona platea</td>
-                        <td>200</td>
-                        <td>300.0</td>
-                        <td>
-                            <a class="btn btn-info" title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
-                        </td>   
-                        <td>
-                             <a class="btn btn-info"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
-                         </td>
-                    </tr>            
-
-                </table>
-
-
+                -->
+                
                 <legend>Agregar Promoción:</legend>                              
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Zona</label>
@@ -174,10 +214,12 @@
                     </div>            
                 </div>  
                 <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10">
+                    <div class="col-sm-offset-10 col-sm-10">
                         <a class="btn btn-info">Agregar</a>
                     </div>
-                </div>                                    
+                </div>                       
+
+
                 <table class="table table-bordered table-striped">
                     <tr>
                         <th>Nombre zona</th>
@@ -218,48 +260,61 @@
                         <th>Editar</th>
                         <th>Eliminar</th>
                     </tr>
-                            <tr>
-                                <td>24/09/2015</td>  
-                                <td>10:30 AM</td>  
-                                <td>10/10/2015</td> 
-                                <td>1:30 PM</td>  
-                                <td>Diario(Lun-Dom)</td>
-                                <td>
-                                    <a class="btn btn-info" title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
-                                </td>                                 
-                                <td>
-                                    <a class="btn btn-info"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>24/09/2015</td>  
-                                <td>2:30 PM</td>  
-                                <td>10/10/2015</td> 
-                                <td>5:30 PM</td>  
-                                <td>Diario(Lun-Vier)</td> 
-                                <td>
-                                    <a class="btn btn-info" title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
-                                </td>                                  
-                                <td>
-                                    <a class="btn btn-info"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
-                                </td>
-                            </tr>
+                    <tr>
+                        <td>24/09/2015</td>  
+                        <td>10:30 AM</td>  
+                        <td>10/10/2015</td> 
+                        <td>1:30 PM</td>  
+                        <td>Diario(Lun-Dom)</td>
+                        <td>
+                            <a class="btn btn-info" title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
+                        </td>                                 
+                        <td>
+                            <a class="btn btn-info"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>24/09/2015</td>  
+                        <td>2:30 PM</td>  
+                        <td>10/10/2015</td> 
+                        <td>5:30 PM</td>  
+                        <td>Diario(Lun-Vier)</td> 
+                        <td>
+                            <a class="btn btn-info" title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
+                        </td>                                  
+                        <td>
+                            <a class="btn btn-info"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
+                        </td>
+                    </tr>
                 </table>
 
-              <div class="form-group">
-                <div class="col-sm-offset-0 col-sm-10">
-                  <a  class="btn btn-info" href="#" >Agregar función</a>
+
+                <div class="form-group">
+                    <div class="col-sm-offset-10 col-sm-10">
+                        <a  class="btn btn-info" href="#" >Agregar</a>
                   
+                    </div>
                 </div>
-              </div>
 
+                <legend>Pol&iacute;ticas</legend>
 
-              <div class="form-group">
-                <div class="col-sm-offset-5 col-sm-10">
-                  <a  class="btn btn-info" href="#"  >Guardar</a>
-                  <button  type="reset" class="btn btn-info">Cancelar</button>
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">Pol&iacute;tica</label>
+                    <div class="col-sm-10">
+                        <select class="form-control" id="sel1">
+                            <option></option>
+                            <option>1</option>
+                            <OPTION>2</OPTION>
+                        </select>
+                    </div>            
+                </div> 
+
+                <div class="form-group">
+                    <div class="col-sm-offset-5 col-sm-10">
+                        <a  class="btn btn-info" href="#"  >Guardar</a>
+                        <button  type="reset" class="btn btn-info">Cancelar</button>
+                    </div>
                 </div>
-              </div>
 
 
             </form>
--- resources/views/internal/promoter/newEvent.blade.php
+++ resources/views/internal/promoter/newEvent.blade.php
@@ -76,7 +76,185 @@
                 <div class="col-sm-10">
                   <input type="file" class="form-control" id="inputEmail3" placeholder="">
                 </div>
-              </div>              
+              </div>         
+              <br>
+
+              <!-- ZONA -->
+              <legend>Agregar zona:</legend>
+              <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
+                  <div class="col-sm-10">
+                      <input type="text" class="form-control" id="input-zone" placeholder="">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Capacidad</label>
+                  <div class="col-sm-10">
+                      <input type="text" class="form-control" id="input-capacity" placeholder="">
+                  </div>
+              </div> 
+
+              <div class="form-group">
+                    <div class="col-sm-offset-10 col-sm-10">
+                        <a class="btn btn-info" onclick="addZone()">Agregar</a>
+                        <!--
+                        <button  type="reset" class="btn btn-info">Cancelar</button>
+                        -->
+                    </div>
+              </div>       
+
+                <script>
+
+                    function addZone(){
+      
+                        var zone = document.getElementById('input-zone').value;
+                        var capacity = document.getElementById('input-capacity').value;
+
+                        var tableRef = document.getElementById('table-zone').getElementsByTagName('tbody')[0];
+
+                        // Insert a row in the table at the last row
+                        var newRow   = tableRef.insertRow(tableRef.rows.length);
+
+                        // Insert a cell in the row at index 0
+                        var newCell  = newRow.insertCell(0);
+                        var newCell2 = newRow.insertCell(1);
+                        var newCell3 = newRow.insertCell(2);
+                        var newCell4 = newRow.insertCell(3);
+
+                        // Append values to cells
+                        var newText  = document.createTextNode(zone);
+                        var newText2 = document.createTextNode(""+capacity);
+                        // buttons
+                        var newEdit = document.createElement('button');
+                        var newDelete = document.createElement('button');
+                        newEdit.className = "btn"; 
+                        newEdit.className += " btn-info glyphicon glyphicon-pencil"; 
+                        newDelete.className = "btn";
+                        newDelete.className += " btn-info glyphicon glyphicon-remove";
+
+                        newCell.appendChild(newText);
+                        newCell2.appendChild(newText2);                        
+                        newCell3.appendChild(newEdit);
+                        newCell4.appendChild(newDelete);
+                
+                    }
+
+                </script>
+
+                <table id="table-zone" class="table table-bordered table-striped ">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Capacidad</th>
+                        <th>Editar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                    <tr>
+                        <td>Zona vip</td>
+                        <td>350.0</td>
+                        <td>
+                            <a class="btn btn-info"  title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
+                        </td>   
+                        <td>
+                             <a class="btn btn-info"   title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
+                         </td>
+                    </tr>
+                    <tr>
+                        <td>Zona platea</td>
+                        <td>300.0</td>
+                        <td>
+                            <a class="btn btn-info" title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
+                        </td>   
+                        <td>
+                             <a class="btn btn-info"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
+                         </td>
+                    </tr>            
+
+                </table>
+                <br>  
+
+                <!-- CLIENTE -->
+
+                <legend>Agregar tipo de cliente:</legend>
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="input-client" placeholder="">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-offset-10 col-sm-10">
+                        <a class="btn btn-info" onclick="addClient()">Agregar</a>
+                        <!--
+                        <button  type="reset" class="btn btn-info">Cancelar</button>
+                        -->
+                    </div>
+                </div>       
+
+                <script>
+
+                    function addClient(){
+      
+                        var cl = document.getElementById('input-client').value;
+
+                        var tableRef = document.getElementById('table-client').getElementsByTagName('tbody')[0];
+
+                        // Insert a row in the table at the last row
+                        var newRow   = tableRef.insertRow(tableRef.rows.length);
+
+                        // Insert a cell in the row at index 0
+                        var newCell  = newRow.insertCell(0);
+                        var newCell2 = newRow.insertCell(1);
+                        var newCell3 = newRow.insertCell(2);
+
+                        // Append values to cells
+                        var newText  = document.createTextNode(cl);
+                        
+                        // buttons
+                        var newEdit = document.createElement('button');
+                        var newDelete = document.createElement('button');
+                        newEdit.className = "btn"; 
+                        newEdit.className += " btn-info glyphicon glyphicon-pencil"; 
+                        newDelete.className = "btn";
+                        newDelete.className += " btn-info glyphicon glyphicon-remove";
+
+                        newCell.appendChild(newText);                        
+                        newCell2.appendChild(newEdit);
+                        newCell3.appendChild(newDelete);
+                
+                    }
+
+                </script>
+
+                <table id="table-client" class="table table-bordered table-striped ">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Editar</th>
+                        <th>Eliminar</th>
+                    </tr>
+                    <tr>
+                        <td>Niño</td>
+                        <td>
+                            <a class="btn btn-info"  title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
+                        </td>   
+                        <td>
+                             <a class="btn btn-info"   title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
+                         </td>
+                    </tr>
+                    <tr>
+                        <td>Adulto</td>
+                        <td>
+                            <a class="btn btn-info" title="Editar"><i class="glyphicon glyphicon-pencil"></i></a>
+                        </td>   
+                        <td>
+                             <a class="btn btn-info"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
+                         </td>
+                    </tr>            
+
+                </table>
+                <br><br><br>
+
+
               <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
                   <a  class="btn btn-info" href="{{url('promoter/event/addFunction')}}"  >Siguiente</a>
@@ -86,8 +264,8 @@
             </form>
           </div>
 
-          
         </div>
+
 
 @stop
 
