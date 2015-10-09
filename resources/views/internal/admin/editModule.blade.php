a93ab7b44f0f872e6c1b149302658b82c111f9c2
luisFFLChannel <a20110982@pucp.pe>
2015-10-08 19:26:33 -0500

    ya

--- resources/views/internal/admin/editModule.blade.php
+++ resources/views/internal/admin/editModule.blade.php
@@ -17,62 +17,35 @@
               <div class="form-group">
                 <label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
                 <div class="col-sm-10">
-                  <!--
                   <input type="text" class="form-control" id="inputEmail3" placeholder="" value="Nombre punto de venta">
-                  -->
-                  {!!Form::text('name', 'Nombre punto de venta' ,['class'=>'form-control','id'=>'inputEmail3','required'])!!}
                 </div>
               </div>
               <div class="form-group">
                 <label for="inputEmail3" class="col-sm-2 control-label">Direción</label>
                 <div class="col-sm-10">
-                  <!--
                   <input type="text" class="form-control" id="inputEmail3" placeholder="" value="Direccion punto de venta">
-                  -->
-                  {!!Form::text('address', 'Direccion punto de venta' ,['class'=>'form-control','id'=>'inputEmail3','required'])!!}
                 </div>
               </div>
               <div class="form-group">
                 <label for="inputEmail3" class="col-sm-2 control-label">Departamento</label>
                 <div class="col-sm-10">
-                  <!--
                   <select class="form-control">
                     <option>Departamento 1</option>
                     <option>Departamento 2</option>
                     <option>Departamento 3</option>
                     <option>Departamento 4</option>
                   </select>
-                  -->
-                  {!!Form::select('select1', [
-                     'op0' => 'Departamento 1',
-                     'op1' => 'Departamento 2',
-                     'op2' => 'Departamento 3',
-                     'op3' => 'Departamento 4'],
-                     null,
-                     ['class' => 'form-control']
-                  )!!}
-
                 </div>
               </div>
               <div class="form-group">
                 <label for="inputEmail3" class="col-sm-2 control-label">Provincia</label>
                 <div class="col-sm-10">
-                  <!--
                   <select class="form-control">
                     <option>Provincia 1</option>
                     <option>Provincia 2</option>
                     <option>Provincia 3</option>
                     <option>Provincia 4</option>
                   </select>
-                  -->
-                  {!!Form::select('select2', [
-                     'op0' => 'Provincia 1',
-                     'op1' => 'Provincia 2',
-                     'op2' => 'Provincia 3',
-                     'op3' => 'Provincia 4'],
-                     null,
-                     ['class' => 'form-control']
-                  )!!}
                 </div>
               </div>
               <div class="form-group">
