49c7557c260e8637b84f27c2a4ad4b87c0573e74
luisFFLChannel <a20110982@pucp.pe>
2015-10-20 10:55:10 -0500

    locales y modificatoria de puntos de venta

--- database/migrations/2015_10_09_203039_create_locals_table.php
+++ database/migrations/2015_10_09_203039_create_locals_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateModulesTable extends Migration
+class CreateLocalsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,18 +12,18 @@ class CreateModulesTable extends Migration
      */
     public function up()
     {
-        Schema::create('modules', function (Blueprint $table) {
+        //
+         Schema::create('locals', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('capacity');
             $table->string('address');
             $table->string('district');
             $table->string('province');
             $table->string('state');
-            $table->string('phone');
-            $table->string('email');
-            $table->timestamp('starTime');
-            $table->timestamp('endTime');
             $table->string('image');
+            $table->integer('row');
+            $table->integer('column');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -36,6 +36,6 @@ class CreateModulesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('modules');
+        //
     }
 }
