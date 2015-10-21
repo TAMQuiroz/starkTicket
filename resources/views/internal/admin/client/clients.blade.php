b0b5d59e470ee7ae795914ead9b3d4b394c33856
axbravo <axbravo@pucp.pe>
2015-10-21 16:01:55 -0500

    client

--- resources/views/internal/admin/client/clients.blade.php
+++ resources/views/internal/admin/client/clients.blade.php
@@ -17,15 +17,19 @@
                 <th>Número de Documento</th>
                 <th>Teléfono</th>
                 <th>Detalle</th>
-                <th>Eliminar</th>
+                <th>Desactivar</th>
             </tr>
 
             @foreach($clients as $client)
             <tr>
                 <td>{{$client->lastname}}, {{$client->name}}</td>
-                <td>Contenido 2</td>
-                <td>Contenido 3</td>
-                <td>Contenido 4</td>
+                <td>@if($client->di_type == 1)
+                    DNI
+                    @else
+                    Carnet de Extranjeria
+                    @endif</td>
+                <td>{{$client->di}}</td>
+                <td>{{$client->phone}}</td>
                 <td><a class="btn btn-info" href="#" title="Detalles"  data-toggle="modal" data-target="#edit"><i class="glyphicon glyphicon-plus"></i></a>
 
                     <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
@@ -33,15 +37,24 @@
                         <div class="modal-content">
                           <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title" id="myModalLabel">Detalle del promotor</h4>
+                            <h4 class="modal-title" id="myModalLabel">Detalle del cliente</h4>
                           </div>
                           <div class="modal-body">
                             <h4>Nombre</h4>
-                            <h4>Sexo</h4>
+                            {{$client->lastname}}, {{$client->name}}
                             <h4>Documento Identidad</h4>
+                            @if($client->di_type == 1)
+                            DNI
+                            @else
+                            Carnet de Extranjeria
+                            @endif
                             <h4>Número de Documento</h4>
+                            {{$client->di}}
                             <h4>Teléfono</h4>
-                            <h4>Direccion</h4>
+                            {{$client->phone}}
+                            
+
+
                           </div>
                           <div class="modal-footer">
                             <button type="button" class="btn btn-info" data-dismiss="modal">Cerrar</button>
@@ -51,8 +64,25 @@
                     </div>
                 </td>
                 <td>
-                  <a class="btn btn-info" href="#"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a>
+                 <a class="btn btn-info" href="" title="Eliminar" data-toggle="modal" data-target="#deleteModal{{$client->id}}"><i class="glyphicon glyphicon-remove"></i></a>
                 </td>
+                <div class="modal fade"  id="deleteModal{{$client->id}}">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">¿Estas seguro que desea dar de baja al cliente?</h4>
+                  </div>
+                  <div class="modal-body">
+                    <h5 class="modal-title">Los cambios serán permanentes</h5>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-info" data-dismiss="modal">No</button>
+                      <a class="btn btn-info" href="{{url('admin/admin/'.$client->id.'/delete')}}" title="Delete" >Sí</a>
+                  </div>
+                </div><!-- /.modal-content -->
+              </div><!-- /.modal-dialog -->
+            </div><!-- /.modal -->
             </tr>
             @endforeach
         </table>
