43a8b7b103988d71a60ff75a143f1c2cd43e07ea
valeria <ug1123@hotmail.com>
2015-10-21 20:49:10 -0500

    modals in Organizer and Promoter

--- resources/views/internal/admin/gifts.blade.php
+++ resources/views/internal/admin/gifts.blade.php
@@ -56,7 +56,7 @@
           </div>
           <div class="modal-footer">
               <button type="button" class="btn btn-info" data-dismiss="modal">No</button>
-              <a class="btn btn-info" href="{{url('admin/gifts/'.$gift->id.'/delete')}}" title="Delete" >Sí</a>
+              <a class="btn btn-info" href="{{url('promoter/organizer/'.$gift->id.'/delete')}}" title="Delete" >Sí</a>
           </div>
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
