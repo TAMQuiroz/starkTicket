729fe9d3ac596302ac1c422fd3ed04ebc13ef490
Juan E <hm.erajuan@gmail.com>
2015-10-27 18:06:33 -0500

    Print sub category title

--- resources/views/external/subcategories.blade.php
+++ resources/views/external/subcategories.blade.php
@@ -35,7 +35,7 @@
         @foreach ($subcategories as $subcategory)
         <div class="col-sm-3">
             {!! Html::image('images/pics13.jpg', null, array('class'=>'image cat_img')) !!}
-            <h3>Subcategoria 1</h3>
+            <h3>{{$subcategory->name}}</h3>
             <p><a href="subcategory/{{$subcategory->id}}" class="btn btn-primary" role="button" data-target="#info" data-whatever="@mdo">Ver más</a></p>
         </div>
         @endforeach
