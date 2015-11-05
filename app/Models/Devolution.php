56914418992cd446b5db1da7f7ad307d7b4e6b6f
Juan E <hm.erajuan@gmail.com>
2015-11-05 15:38:18 -0500

    Model Devolution and Error on devolution controller fixed

--- app/Models/Devolution.php
+++ app/Models/Devolution.php
@@ -3,27 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
-
-
 
 class Devolution extends Model
 {
+    use SoftDeletes;
 
-	use SoftDeletes;
     protected $table = 'devolutions';
     protected $dates = ['deleted_at'];
 
-    public function ticket()
-    {
-        return $this->belongsTo('App\Models\Ticket','ticket_id');
-    }
-
-    public function administrator()
-    {
+    public function administrator(){
         return $this->belongsTo('App\User','user_id');
     }
 
+    public function ticket(){
+    	return $this->belongsTo('App\Models\Ticket','ticket');
+    }
 }
