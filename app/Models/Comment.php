5fdd769f18512d3908372274a8cfd4a68217028c
Jose <jemarroquin@pucp.pe>
2015-11-06 14:34:05 -0500

    Feedback cliente y asistencia Vendedores

---
+++ app/Models/Comment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Comment extends Model
+{
+   use SoftDeletes;
+    protected $table = 'comment';
+    protected $dates = ['deleted_at'];
+
+
+
+
+
+
+// protected $fillable = ['id', 'user_id', 'event_id', 'description' , 'time' ] ;
+
+
+  public function event(){
+        return $this->belongsTo('App\Models\Event','event_id');
+    }
+
+    public function user(){
+        return $this->belongsTo('App\User','user_id');
+    }
+
+
+}
