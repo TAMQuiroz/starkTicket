f03e0f2bdd061d7631a2be3b7833741d52ee3e5a
Juan E <hm.erajuan@gmail.com>
2015-11-06 18:49:55 -0500

    cancel event migrate,y campo canceled en event y ticket

--- database/migrations/2015_10_18_162234_create_tickets_table.php
+++ database/migrations/2015_10_18_162234_create_tickets_table.php
@@ -17,7 +17,7 @@ class CreateTicketsTable extends Migration
             $table->timestamp('payment_date')->nullable();
             $table->string('reserve');
             $table->timestamp('refund_date')->nullable();
-            $table->integer('cancelled');
+            $table->boolean('canceled')->default(false);
             $table->integer('owner_id')->unsigned()->nullable();
             $table->integer('event_id')->unsigned();
             $table->integer('presentation_id')->unsigned();
