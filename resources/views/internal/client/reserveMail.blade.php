9db3cbbbdda866343ad21cbd22ee6648704c7fe0
caluuvi <claudia.villanuevac@pucp.pe>
2015-11-13 10:26:05 -0500

    reserve mail

--- resources/views/internal/client/reserveMail.blade.php
+++ resources/views/internal/client/reserveMail.blade.php
@@ -2,7 +2,6 @@ CONFIRMACIÓN DE RESERVA
 
 A continuación te mostramos los datos.
 
-Pedido realizado el  <br>
 Evento: {{$tickets->first()->event->name}} <br>
 Fecha: {{$tickets->first()->eventDate}} <br>
 Zona: {{tickets->first()->zone->name}} <br>
