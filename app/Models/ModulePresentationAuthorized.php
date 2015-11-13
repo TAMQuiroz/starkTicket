025b01a795d0c90375908c4498981e9d9fa4019d
Juan E <hm.erajuan@gmail.com>
2015-11-13 13:05:54 -0500

    ModulePresentationAuthorized

---
+++ app/Models/ModulePresentationAuthorized.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModulePresentationAuthorized extends Model
+{
+    protected $table = 'module_presentation_authorized';
+}
