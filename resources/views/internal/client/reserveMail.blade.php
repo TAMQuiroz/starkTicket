82a18beae7f0bb98e311f6166142958e7b7ee329
TAMQuiroz <tamquiroz@gmail.com>
2015-11-13 20:31:33 -0500

    :(

--- resources/views/internal/client/reserveMail.blade.php
+++ resources/views/internal/client/reserveMail.blade.php
@@ -4,4 +4,4 @@ A continuación te mostramos los datos.
 
 Evento: {{$tickets->first()->event->name}} <br>
 Fecha: {{$tickets->first()->eventDate}} <br>
-Zona: {{$tickets->first()->zone->name}} <br>
+Zona: {{$tickets->first()->zone->name}}  <br>
