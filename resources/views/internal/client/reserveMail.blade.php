5bb9fa9552c778db13e75cb4d8634bdcadca9aea
caluuvi <claudia.villanuevac@pucp.pe>
2015-11-14 23:16:29 -0500

    fixing email detail

--- resources/views/internal/client/reserveMail.blade.php
+++ resources/views/internal/client/reserveMail.blade.php
@@ -1,7 +1,26 @@
-CONFIRMACIÓN DE RESERVA
+<h5>CONFIRMACIÓN DE RESERVA</h5>
 
-A continuación te mostramos los datos.
+<div>A continuación te mostramos los datos.</div>
 
-Evento: {{$tickets->first()->event->name}} <br>
-Fecha: {{$tickets->first()->eventDate}} <br>
-Zona: {{tickets->first()->zone->name}} <br>
+<table class="table table-bordered" style="width:1px">
+	<thead>
+	    <tr>
+	        <th>Evento</th>
+	        <th>Fecha y Hora</th>
+	        <th>Zona</th>
+	        <th>Cantidad</th>
+	        <!--<th>Ubicación</th>
+	        <th>Promocion</th>
+	        <th>Precio Unitario</th>
+	        <th>Precio Total</th>-->
+	    </tr>
+	</thead>
+	<tbody>
+	    <tr>
+	    	<td>{{$tickets->first()->event->name}}</td>
+	    	<td>{{$tickets->first()->eventDate}}</td>
+	    	<td>{{$tickets->first()->zone->name}}</td>
+	    	<td>{{$tickets->first()->cant}}</td>
+	    </tr>
+	</tbody>
+</table>
