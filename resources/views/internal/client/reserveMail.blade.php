96964d86310f5ec0305b940a5013b00a2c7e6d5f
caluuvi <claudia.villanuevac@pucp.pe>
2015-11-14 23:34:23 -0500

    edit email detail

--- resources/views/internal/client/reserveMail.blade.php
+++ resources/views/internal/client/reserveMail.blade.php
@@ -9,18 +9,25 @@
 	        <th>Fecha y Hora</th>
 	        <th>Zona</th>
 	        <th>Cantidad</th>
-	        <!--<th>Ubicación</th>
+	        <!--<th>Ubicación</th>-->
 	        <th>Promocion</th>
 	        <th>Precio Unitario</th>
-	        <th>Precio Total</th>-->
+	        <th>Precio Total</th>
 	    </tr>
 	</thead>
 	<tbody>
 	    <tr>
 	    	<td>{{$tickets->first()->event->name}}</td>
-	    	<td>{{$tickets->first()->eventDate}}</td>
+	    	<td>{{$tickets->first()->presentation->starts_at}}</td>
 	    	<td>{{$tickets->first()->zone->name}}</td>
-	    	<td>{{$tickets->first()->cant}}</td>
+	    	<td>{{$tickets->first()->quantity}}</td>
+	        @if($tickets->first()->promo)
+	        <td>{{$tickets->first()->promo->name}}</td>
+	        @else
+	        <td>No tiene</td>
+	        @endif
+	        <td>S/. {{$tickets->first()->price}}</td>
+	        <td>S/. {{$tickets->first()->total_price}}</td>
 	    </tr>
 	</tbody>
 </table>
