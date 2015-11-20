305f229a0534f7713d111a825bdb9029831e8889
Juan E <hm.erajuan@gmail.com>
2015-11-20 15:23:41 -0500

    Default cero las comisiones, mensaje de confirmacion al guardar

--- resources/views/internal/promoter/payment/payments.blade.php
+++ resources/views/internal/promoter/payment/payments.blade.php
@@ -9,40 +9,7 @@
 @stop
 
 @section('content')
-
-
-<!-- Contenido-->
-  <!--div class="row">
-          <div class="col-sm-8">
-            <form class="form-horizontal" method="post">
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Fecha inicio</label>
-                    <div class="col-sm-10">
-                        {!! Form::date('dateIni','', array('class' => 'form-control')) !!}
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label  class="col-sm-2 control-label">Fecha fin</label>
-                    <div class="col-sm-10">
-                        {!! Form::date('dateEnd','', array('class' => 'form-control')) !!}
-                    </div>
-                </div>
-              <div class="form-group">
-                  <label class="col-sm-2 control-label">Organizador</label>
-                  <div class="col-sm-10">
-                    {!! Form::select('organizer', ['Pepitos Producciones','Rayo en la botella','Hermanos yaipen'],null,['class' => 'form-control']) !!}
-                  </div>
-              </div>
-                <div class="form-group">
-                    <div class="col-sm-offset-10 col-sm-10">
-                        <a class="btn btn-info">Buscar</a>
-                    </div>
-                </div>
-            </form>
-          </div>
-  </div-->
-
-      <table class="table table-bordered table-striped">
+     <table class="table table-bordered table-striped">
         <tr>
           <th>Evento</th>
           <th>Organizdor</th>
