6c9caa98f1beefc02e482d9aa09592de57a2b05d
Juan E <hm.erajuan@gmail.com>
2015-11-26 18:36:28 -0500

    Actulizacion de plantillas de password, mas front

--- resources/views/internal/client/home.blade.php
+++ resources/views/internal/client/home.blade.php
@@ -2,27 +2,6 @@
 
 @section('style')
 	{!!Html::style('css/images.css')!!}
-	<style type="text/css">
-		.btn-primary{
-            background-color: #83D3C9;
-            border-color: #83D3C9;
-        }
-        .btn-primary:hover{
-            background-color: #329DB7;
-            border-color: #329DB7;
-        }
-        .row h3, .row p{
-        	text-align: center;
-        }
-        section .caption{
-        	float:left;
-        }
-
-        .caption h3, .caption p{
-        	text-align: left;
-        }
-
-	</style>
 @stop
 
 @section('title')
@@ -30,11 +9,8 @@
 @stop
 
 @section('content')
-
-<div><h4>Eventos Recomendados</h4></div>
-
+<h4>Eventos Recomendados</h4>
 <div class="row">
-
 	@foreach($clientPreferences as $clientPreference)
   	<div class="col-sm-6 col-md-4">
 	    <div >
@@ -49,20 +25,15 @@
 	        	<b>Fecha de venta: </b> {{date('Y-m-d',$clientPreference->selling_date)}}<br>
                 <b>Lugar: </b> {{$clientPreference->place->name}} <br>
                 <b>Direccion:</b> {{$clientPreference->place->address}} <br>
-
 	        </p>
-	        <p><a href="{{url('event/'.$clientPreference->id.'')}}" class="btn btn-primary" role="button">Detalle</a></p>'
+	        <p><a href="{{url('event/'.$clientPreference->id.'')}}" class="btn btn-info" role="button">Detalle</a></p>
 		   </div>
 	      </section>
 	    </div>
   	</div>
   	@endforeach
-
-
 </div>
 @stop
-
-
 @section('javascript')
 
 @stop
