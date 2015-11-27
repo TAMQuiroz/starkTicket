58d360a38c03c8d30931519d8450e894ead2afdb
Juan E <hm.erajuan@gmail.com>
2015-11-26 19:38:46 -0500

    Codigo comentado adios, libreria sin uso adios

--- resources/views/external/modules.blade.php
+++ resources/views/external/modules.blade.php
@@ -1,14 +1,7 @@
 @extends('layoutExternal')
 
 @section('style')
-	{!!Html::style('css/skeletoGift.css')!!}
-	{!!Html::style('css/estiloGift.css')!!}
   {!!Html::style('css/images.css')!!}
-  <style type="text/css">
-        #nav .fifth{
-            color: white;
-        }
-  </style>
 @stop
 
 @section('title')
@@ -16,95 +9,7 @@
 @stop
 
 @section('content')
-	<!-- Main -->
-   <!--
-		<div id="main">
-			<div class="container">
-				<div class="row">
-				
-					
-						<div id="content" class="8u skel-cell-important">
-							<section>
-								<header>
-									<h2 class = "prueba4">Ubicación de nuestros puntos de venta</h2>
-								</header>
-								
-                                <iframe width="770" height="480" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=Avenida%20Ant%C3%BAnez%20De%20Mayolo%2C%20Los%20Olivos%2C%20Lima%2C%20Peru&key=AIzaSyBQwG1d7QYu_dbedftXgRCFLsB24xCbHDk" allowfullscreen></iframe>
-							</section>
-
-                            
-                               <div class="col-md-6"> 
-                     
-                                </div>
-						</div>
-			
-                    
-                    
-						
-		
-						<div id="sidebar" class="4u">
-							<section class="scrol">
-              <style>.scrol{
-                  width: 100%;
-                  height: 650px;
-                  overflow: scroll;
-              }</style>
-								<header>
-									<h2 class = "title">Puntos de Venta</h2>
-                                   							
-                                </header>
-								<p>Descubre los puntos de venta mas cercanos a tu distrito para que puedas realizar
-                                tu compra de una manera mucho mas rápida y efectiva</p>
-                                
-                            <h3 class = "prueba1">Mercado Covida</h3>
-                                   <p>
-                                        Av. Antunez de Mayolo<br>1247, Covida - Los Olivos<br>
-                                   </p>
-                                   <p><i class="fa fa-phone"></i> 
-                                       <abbr title="Phone">P</abbr>: (511) 521-1621</p>
-                                   <p><i class="fa fa-envelope-o"></i> 
-                                       <abbr title="Email">E</abbr>: <a class = "prueba3">eleticket@oficial.pe</a>
-                                   </p>
-                                   <p><i class="fa fa-clock-o"></i> 
-                                       <abbr title="Hours">H</abbr>: Lunes - Viernes: 9:00AM-5:00 PM</p>
-
-                               <h3 class = "prueba1">Cine Imperio</h3>
-                                   <p>
-                                        Av. Tacna<br>255, Distrito de Lima<br>
-                                   </p>
-                                   <p><i class="fa fa-phone"></i>   
-                                       <abbr title="Phone">P</abbr>: (511) 541-1021</p>
-                                   <p><i class="fa fa-envelope-o"></i> 
-                                       <abbr title="Email">E</abbr>: <a class = "prueba3">eleticket@oficial.pe</a>
-                                   </p>
-                                   <p><i class="fa fa-clock-o"></i> 
-                                       <abbr title="Hours">H</abbr>: Lunes - Viernes: 9:00AM-5:00 PM</p>
-
-                                  <h3 class = "prueba1">Cine Tacna Prime 5 stars</h3>
-                                   <p>
-                                        Av. Tacna<br>667, Distrito de Lima<br>
-                                   </p>
-                                   <p><i class="fa fa-phone"></i>   
-                                       <abbr title="Phone">P</abbr>: (511) 541-1021</p>
-                                   <p><i class="fa fa-envelope-o"></i> 
-                                       <abbr title="Email">E</abbr>: <a class = "prueba3">eleticket@oficial.pe</a>
-                                   </p>
-                                   <p><i class="fa fa-clock-o"></i> 
-                                       <abbr title="Hours">H</abbr>: Lunes - Viernes: 9:00AM-5:00 PM</p
-                                      
-								
-							</section>
-						</div>
-					
-						
-				</div>
-			
-			</div>
-		</div> -->
-    <header>
     <h3>Ubica nuestros Módulos de Venta </h3>
-
-  </header>
     <table class="table table-bordered table-striped">
         <tr>
             <th>Nombre</th>
@@ -122,7 +27,7 @@
           <td>{{$module->email}}</td>
           <td>{{$module->phone}}</td>
           <td>
-            <a class="btn btn-info" href="detalles" title="Detalles" data-toggle="modal" data-target="#edit{{$module->id}}"><i class="glyphicon glyphicon-plus"></i></a> 
+            <a class="btn btn-info" href="detalles" title="Detalles" data-toggle="modal" data-target="#edit{{$module->id}}"><i class="glyphicon glyphicon-plus"></i></a>
             <div class="modal fade" id="edit{{$module->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
               <div class="modal-dialog" role="document">
                 <div class="modal-content">
@@ -156,11 +61,7 @@
             </div>
           </td>
           @endforeach
-
-
-
     </table>
-  
 @stop
 
 @section('javascript')
--- resources/views/internal/admin/locals/locals.blade.php
+++ resources/views/internal/admin/locals/locals.blade.php
@@ -20,46 +20,15 @@
                 <th>Editar</th>
                 <th>Eliminar</th>
             </tr>
-            <!--
-            <tr>
-                <td>Categoria 1</td>
-                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
-                <td>323</td>
-                <td>{!! Html::image('images/map.png', null, array('class'=>'gift_img')) !!}</td>
-                <td><a class="btn btn-info" href="{{url('admin/local/1/edit')}}" title="Editar" ><i class="glyphicon glyphicon-pencil"></i></a>
-                </td> 
-                <td><a class="btn btn-info" href="#"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a> </td>
-            </tr>
-            <tr>
-                <td>Categoria 2</td>
-                <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
-                <td>300</td>
-                <td>{!! Html::image('images/map.png', null, array('class'=>'gift_img')) !!}</td>
-               <td><a class="btn btn-info" href="{{url('admin/local/1/edit')}}" title="Editar" ><i class="glyphicon glyphicon-pencil"></i></a>
-                </td> 
-                <td><a class="btn btn-info" href="#"  title="Eliminar" ><i class="glyphicon glyphicon-remove"></i></a> </td>
-            </tr>
-
-          -->
           @foreach($locals as $local)
           <tr>
             <td>{{$local->name}}</td>
             <td>{{$local->address}}</td>
             <td>{{$local->capacity}}</td>
-            
+
             <td>{!! Html::image($local->image, null, array('class'=>'gift_img')) !!}</td>
             <td>
-              <a class="btn btn-info" href="detalles" title="Detalles" data-toggle="modal" data-target="#edit{{$local->id}}"><i class="glyphicon glyphicon-plus"></i></a> 
+              <a class="btn btn-info" href="detalles" title="Detalles" data-toggle="modal" data-target="#edit{{$local->id}}"><i class="glyphicon glyphicon-plus"></i></a>
               <div class="modal fade" id="edit{{$local->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
                 <div class="modal-dialog" role="document">
                   <div class="modal-content">
@@ -73,13 +42,7 @@
                       <h4>Capacidad: </h4>
                       {{$local->capacity}}
                       <h4>Direccion: </h4>
-                      {{$local->address}}
-                      <h4>Distrito: </h4>
-                      {{$local->district}}
-                      <h4>Provincia: </h4>
-                      {{$local->province}}
-                      <h4>Departamento: </h4>
-                      {{$local->state}}
+                      {{$local->address}}, {{$local->district}}, {{$local->province}}, {{$local->state}}
                       <br>
                       <br>
                       {!! Html::image($local->image, null, array('class'=>'modal_img')) !!}
@@ -94,7 +57,7 @@
 
             <td>
               <a class="btn btn-info" href="{{url('admin/local/'.$local->id.'/edit')}}" title="Editar" ><i class="glyphicon glyphicon-pencil"></i></a>
-            </td> 
+            </td>
             <td>
               <a class="btn btn-info" href=""  title="Eliminar" data-toggle="modal" data-target="#deleteModal{{$local->id}}" ><i class="glyphicon glyphicon-remove"></i></a>
             </td>
@@ -117,30 +80,8 @@
             </div><!-- /.modal-dialog -->
           </div><!-- /.modal -->
           @endforeach
-
-
         </table>
-        {!!$locals->render()!!} 
-        <!--
-        <nav>
-            <ul class="pagination">
-              <li>
-                <a href="#" aria-label="Previous">
-                  <span aria-hidden="true">&laquo;</span>
-                </a>
-              </li>
-              <li><a href="#">1</a></li>
-              <li><a href="#">2</a></li>
-              <li><a href="#">3</a></li>
-              <li><a href="#">4</a></li>
-              <li><a href="#">5</a></li>
-              <li>
-                <a href="#" aria-label="Next">
-                  <span aria-hidden="true">&raquo;</span>
-                </a>
-              </li>
-            </ul>
-          </nav> -->
+        {!!$locals->render()!!}
 @stop
 
 @section('javascript')
